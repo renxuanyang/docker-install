b8d91a468e4c10a5abae342e37f8735c6443856d
renxuanyang <5106432+renxuanyang@users.noreply.github.com>
2024-01-11 10:56:56 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -9,8 +9,8 @@ sudo apt-get autoremove
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
 sudo groupadd docker
